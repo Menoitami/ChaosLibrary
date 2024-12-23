bd4a80b8ea78f1fe4a4ba7af457b371f7a888ffc
Menoitami <lalacfd@gmail.com>
2024-12-23 20:21:13 +0300

    bifurinit

---
+++ bifurcationHOST.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+
+#include <iomanip>
+#include <string>
+
+
+
+__host__ void bifurcation1D(
+	const double	tMax,							
+	const int		nPts,							
+	const double	h,								
+	const int		amountOfInitialConditions,		
+	const double*	initialConditions,				
+	const double*	ranges,							
+	const int*		indicesOfMutVars,				
+	const int		writableVar,					
+	const double	maxValue,						
+	const double	transientTime,					
+	const double*	values,							
+	const int		amountOfValues,					
+	const int		preScaller,
+	std::string		OUT_FILE_PATH);	
